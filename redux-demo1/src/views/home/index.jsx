f81b7b73163d9bcb53b43b28fab565742109fb10
llqfront <260137290@qq.com>
2019-02-21 22:30:28 +0800

    up

--- redux-demo1/src/views/home/index.jsx
+++ redux-demo1/src/views/home/index.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+
+import { withRouter,NavLink} from 'react-router-dom';
+
 class View extends Component {
     render() {
         return (
             <div className="home">
                home
+               <NavLink to="/">home</NavLink>
+               <NavLink to="/test">test</NavLink>
             </div>
         )
     }
--- redux-demo1/src/views/test/index.jsx
+++ redux-demo1/src/views/test/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { NavLink} from 'react-router-dom';
 class View extends Component {
     render() {
         return (
             <div className="home">
                test
+               <NavLink to="/">home</NavLink>
+               <NavLink to="/test">test</NavLink>
             </div>
         )
     }

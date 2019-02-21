1adaf712ba90671da22b3b0665ff4ab8c50bf612
llqfront <260137290@qq.com>
2019-02-21 22:23:51 +0800

    up

---
+++ redux-demo1/src/views/home/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class View extends Component {
+    render() {
+        return (
+            <div className="home">
+               home
+            </div>
+        )
+    }
+}
+export default View;
---
+++ redux-demo1/src/views/test/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class View extends Component {
+    render() {
+        return (
+            <div className="home">
+               test
+            </div>
+        )
+    }
+}
+export default View;

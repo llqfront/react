718b86520d16252af3b052a5524637af70c342ca
llqfront <260137290@qq.com>
2019-02-28 20:30:03 +0800

    up

--- redux-demo1/src/App.js
+++ redux-demo1/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter,Switch,Route} from 'react-router-dom';
-// import home from './views/home/';
-// import test from './views/test/';
 import lazyLoad from './lazyLoad';
+import { Provider } from 'mobx-react';
+// import { observable, useStrict ,autorun} from 'mobx'
+
+import otcStore from './store/otc';
 
 const home = lazyLoad(() => import(/* webpackChunkName: "Home" */ './views/home/'));
 const test = lazyLoad(() => import(/* webpackChunkName: "Test" */ './views/test/'));
 
+
+// const cnstore = new cnStore();
+const stores = {
+  otc: new otcStore(),
+  // ...other stores
+};
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/test" component={test} />
-        </Switch>
-      </BrowserRouter>
+      <Provider {...stores}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/test" component={test} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- redux-demo1/src/views/home/index.jsx
+++ redux-demo1/src/views/home/index.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import {observer,inject} from 'mobx-react';
 import { withRouter,NavLink} from 'react-router-dom';
-
+// 导入需要的模块
+@withRouter
+@inject('otc')
+@observer
 class View extends Component {
     render() {
+        console.log(this.props.otc)
         return (
             <div className="home">
                home<br/>
